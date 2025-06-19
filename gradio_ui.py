4b28aae7694e9fd8970f63a9ba75648e686ffe8d
Naman yadav <namany7927@gmail.com>
2025-06-19 21:25:50 +0530

    Update gradio_ui.py

--- gradio_ui.py
+++ gradio_ui.py
@@ -78,7 +78,7 @@ with gr.Blocks(css=".gr-box { border-radius: 12px; padding: 16px; box-shadow: 0 
 
         with gr.Column(scale=1):
             output_box = gr.Textbox(label="📦 AI-Generated Course Plan", lines=18, interactive=False, show_copy_button=True)
-            txt_file = gr.File(label="📄 Download your TXT", visible=True)
+            txt_file = gr.File(label="📄TXT Generated", visible=false)
 
     submit_btn.click(
         fn=generate,
